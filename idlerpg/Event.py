57b4502ed2b1358bbd5979fe480865e269a74a3f
Ludovic Bellière <ludovic.belliere@xrogaan.be>
2012-05-03 18:06:05 +0200

    WIP: Update event object
    However, it's still crap. I need to design a database schema to
    upgrade known collections. Then, I need to decide if I really want
    to put the events into the database (I probably will)..

--- idlerpg/Event.py
+++ idlerpg/Event.py
@@ -1,24 +1,100 @@
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=80:
-
 """
-TODO: créer une anatomie
-quest t1:
+Event types:
+    * quests (different quest type, see wiki)
+    * object ecounters
+    * monster ecounters
+    * calamities
+    * hand of gods
+    * godsend
 
 handofgod:
     text: "%s god blessed by ..."
+
+clock: internal clock. Something happen every clock second.
 """
 
-class Event:
-    def __init__(self):
-        self.type=None
-        self.text=None
-        self.opts=[]
-        self.odds=None
+import logging
+from pymongo import Database
 
-    def get_odds(self):
-        import random
-        return random.randint((self.odds*86400)/clock)
+class Event(object):
+    def __init__(self, clock, etype, text, odds, oid, **kwargs):
+        self._logger = logging.getLogger('Event.Event')
+        self.clock = clock
+        self.etype = etype
+        self.text = text
+        self.odds = int(odds)
+        self.oid = oid
+        self.options = kwargs
+        return 1
 
-    def get_text(self, people=[]i):
-        return self.text % tuple(people)
+    @properties
+    def odds(self):
+        from random import randint
+        return randint(0, (self._odds*86400)/self.clock)
 
+    @odds.setter
+    def odds(self, value):
+        value = int(value)
+        if value < 1:
+            value = 1
+            self._logger.warn('odds must be greater or equal to 1.')
+        self._odds = value
+
+    @odds.deleter
+    def odds(self):
+        del self._odds
+
+    def get_text(self):
+        return self.text
+
+class EventMasterList(object):
+    """
+    Contain a list of every event.
+    """
+
+    def __init__(self, databaseObj, config):
+        self._types = ('calamity', 'hog', 'godsend', 'object_find',
+                       'monster_encounter')
+        self._documentObj = databaseObj.events
+        self._events = dict()
+        for calamity in self._documentObj.find({'type':'calamity'}):
+            if not self._events.has_key('calamity'):
+                self._events.update({'calamity': list()})
+            Event(clock=config.clock, **calamity)
+            self._events['calamity'].append(Event(clock=config.clock,
+                                                  **calamity))
+
+class EventManager(object):
+    version = 1.0
+    def __init__(self, databaseObj, config):
+        self._logger = logging.getLogger('Event.EventManager')
+        if not isinstance(databaseObj, Database):
+            self._logger.error('Require an instance of '\
+                               'pymongo.database.Database to work. Got %s.',
+                               type(databaseObj))
+        self._databaseObj = databaseObj
+        self._eventsObj = databaseObj.events
+
+        version = self._eventsObj.find_one({}, {'version':1})
+        if not version:
+            self.__start_install()
+        elif float(version) < self.version:
+            self.__start_upgrade(version)
+
+        self._master = EventMasterList(self._databaseObj, config)
+
+    def __start_install(self):
+        pass
+
+    def __start_upgrade(self, version):
+        method = '__upgrade_from_' + version + '_to_' + self.version
+        if not hasattr(self, method):
+            self._logger.critical('The Event collection upgrade failed: '\
+                                  "I don't know how to upgrade from %0.2f "\
+                                  'to %0.2f.', (version, self.version))
+            raise RuntimeError('Collection upgrade failed.')
+        getattr(self, method)(version)
+
+    def trigger(self):
+        pass
